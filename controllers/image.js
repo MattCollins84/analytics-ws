1595d8850a2cb407c768edabcf9c79b6719bab9c
MattCollins84 <matt@strikesandgutters.com>
2018-01-23 11:38:21 +0000

    OpenCV packages and using the IPC

--- controllers/image.js
+++ controllers/image.js
@@ -1,11 +1,18 @@
 const { exec } = require('child_process');
 const fs = require('fs');
+const cv = require('opencv4nodejs');
 
+// analyse a single image
 const singleImage = (params, callback) => {
 
+  // tmp filename of original image
   const tmpName = `/tmp/${Date.now()}-${params.image.name}`;
+  // tmp filename of YOLO response data
   const tmpOutput = `${tmpName}-output.txt`;
+  // tmp filename of original image with detections drawn
+  const tmpDetectionsImg = `${tmpName}-output.jpg`;
 
+  // move the uploaded image to the tmp location
   params.image.mv(tmpName, (err) => {
     
     if (err) return callback({
@@ -16,7 +23,9 @@ const singleImage = (params, callback) => {
       ]
     });
 
-    exec(`/home/analytics/code/darknet/darknet ${tmpName} ${tmpOutput}`, (err, stdout, stderr) => {
+    // run YOLO on tmp image
+    exec(`/home/analytics/Projects/IPC/cvImgAnalytics ${tmpName} ${tmpOutput}`, (err, stdout, stderr) => {
+      
       if (err) return callback({
         status: 500,
         errorMessage: "There was a problem analysing this file",
@@ -24,7 +33,10 @@ const singleImage = (params, callback) => {
           err
         ]
       });
+
+      // read the output data
       fs.readFile(tmpOutput, (err, data) => {
+        
         if (err) return callback({
           status: 500,
           errorMessage: "There was a problem reading the analysis output",
@@ -32,9 +44,42 @@ const singleImage = (params, callback) => {
             err
           ]
         });
-        return callback(null, data.toString('utf8'));
+
+        // parse output data?
+        console.log(data);
+
+        // load the image into opencv
+        // cv.imreadAsync(tmpName, (err, outputImg) => {
+          
+        //   if (err) return callback({
+        //     status: 500,
+        //     errorMessage: "There was a problem creating the output image",
+        //     errors: [
+        //       err
+        //     ]
+        //   });
+
+        //   // blue
+        //   const colour = new cv.Vec3(255, 0, 0);
+          
+        //   // capture the detections and draw a rectangle for each
+        //   const rectangles = data.detections;
+        //   rectangles.forEach(rectangle => {
+        //     const position = new cv.Rect(x, y, width, height);
+        //     outputImg.drawRectangle(position, colour);
+        //   });
+
+        //   // write to tmp detections image
+        //   cv.imwriteAsync(tmpDetectionsImg, outputImg, (err) => {
+            return callback(null, data.toString('utf8'));
+          // });
+
+        // });
+        
       });
+
     });
+
   });
 
 }
