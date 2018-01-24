00ad0d8690fadf76d38a7718839fd2a7c9a3962d
MattCollins84 <matt@strikesandgutters.com>
2018-01-24 10:28:41 +0000

    annotate param

--- controllers/image.js
+++ controllers/image.js
@@ -60,26 +60,28 @@ const singleImage = (params, callback) => {
           ]
         });
 
-        // blue
-        const red = [0, 0, 255];
-        const green = [0, 255, 0];
-        const blue = [255, 0, 0];
+        if (params.annotate) {
+          // colours
+          const red = [0, 0, 255];
+          const green = [0, 255, 0];
+          const blue = [255, 0, 0];
 
-        // capture the detections and draw a rectangle for each
-        const detections = data.detections;
-        detections.forEach(detection => {
-          outputImg.rectangle([detection.x, detection.y], [detection.w, detection.h], red, 2);
-          outputImg.putText(detection.name, detection.x + 10, detection.y + 15, "HERSEY_SIMPLEX", red, 0.5, 2);
-        });
+          // capture the detections and draw a rectangle for each
+          const detections = data.detections;
+          detections.forEach(detection => {
+            outputImg.rectangle([detection.x, detection.y], [detection.w, detection.h], red, 2);
+            outputImg.putText(detection.name, detection.x + 10, detection.y + 15, "HERSEY_SIMPLEX", red, 0.5, 2);
+          });
 
-        // outputImg.putText("TEST", 25, 20, "HERSEY_SIMPLEX", red, 1, 2);
-        // outputImg.putText("TEST", 25, 120, "HERSEY_SIMPLEX", green, 2, 3);
-        // outputImg.putText("TEST", 25, 220, "HERSEY_SIMPLEX", blue, 4, 4);
+          // outputImg.putText("TEST", 25, 20, "HERSEY_SIMPLEX", red, 1, 2);
+          // outputImg.putText("TEST", 25, 120, "HERSEY_SIMPLEX", green, 2, 3);
+          // outputImg.putText("TEST", 25, 220, "HERSEY_SIMPLEX", blue, 4, 4);
 
-        // write to tmp detections image
-        outputImg.save(tmpDetectionsImg);
+          // write to tmp detections image
+          outputImg.save(tmpDetectionsImg);
 
-        data.imgUrl = `/${ts}-${params.image.name}`;
+          data.imgUrl = `/${ts}-${params.image.name}`;
+        }
 
         return callback(null, data);
 
