d19537b9e6f6bbc872bd1d443999728f8a1d0775
MattCollins84 <matt@strikesandgutters.com>
2018-01-24 10:18:15 +0000

    remove debug;

--- controllers/image.js
+++ controllers/image.js
@@ -36,70 +36,54 @@ const singleImage = (params, callback) => {
         ]
       });
 
-      console.log("STD OUT")
-      console.log(stdout);
-
-      // read the output data
-      // fs.readFile(tmpOutput, (err, data) => {
-        
-      //   if (err) return callback({
-      //     status: 500,
-      //     errorMessage: "There was a problem reading the analysis output",
-      //     errors: [
-      //       err
-      //     ]
-      //   });
-
-        // parse output data
-        try {
-          data = JSON.parse(stdout.toString('utf8'))
-        } catch (e) {
-          return callback({
-            status: 500,
-            errorMessage: "An unexpected error occured",
-            errors: [
-              e
-            ]
-          });
-        }
-
-        // load the image into opencv
-        cv.readImage(tmpName, (err, outputImg) => {
-          
-          if (err) return callback({
-            status: 500,
-            errorMessage: "There was a problem loading the output image",
-            errors: [
-              err
-            ]
-          });
-
-          // blue
-          const red = [0, 0, 255];
-          const green = [0, 255, 0];
-          const blue = [255, 0, 0];
-
-          // capture the detections and draw a rectangle for each
-          const detections = data.detections;
-          detections.forEach(detection => {
-            outputImg.rectangle([detection.x, detection.y], [detection.w, detection.h], red, 2);
-            outputImg.putText(detection.name, detection.x + 10, detection.y + 15, "HERSEY_SIMPLEX", red, 0.5, 2);
-          });
-
-          // outputImg.putText("TEST", 25, 20, "HERSEY_SIMPLEX", red, 1, 2);
-          // outputImg.putText("TEST", 25, 120, "HERSEY_SIMPLEX", green, 2, 3);
-          // outputImg.putText("TEST", 25, 220, "HERSEY_SIMPLEX", blue, 4, 4);
-
-          // write to tmp detections image
-          outputImg.save(tmpDetectionsImg);
-
-          data.imgUrl = `/${ts}-${params.image.name}`;
-
-          return callback(null, data);
-
+      // parse output data
+      try {
+        data = JSON.parse(stdout.toString('utf8'))
+      } catch (e) {
+        return callback({
+          status: 500,
+          errorMessage: "An unexpected error occured",
+          errors: [
+            e
+          ]
         });
+      }
+
+      // load the image into opencv
+      cv.readImage(tmpName, (err, outputImg) => {
         
-      // });
+        if (err) return callback({
+          status: 500,
+          errorMessage: "There was a problem loading the output image",
+          errors: [
+            err
+          ]
+        });
+
+        // blue
+        const red = [0, 0, 255];
+        const green = [0, 255, 0];
+        const blue = [255, 0, 0];
+
+        // capture the detections and draw a rectangle for each
+        const detections = data.detections;
+        detections.forEach(detection => {
+          outputImg.rectangle([detection.x, detection.y], [detection.w, detection.h], red, 2);
+          outputImg.putText(detection.name, detection.x + 10, detection.y + 15, "HERSEY_SIMPLEX", red, 0.5, 2);
+        });
+
+        // outputImg.putText("TEST", 25, 20, "HERSEY_SIMPLEX", red, 1, 2);
+        // outputImg.putText("TEST", 25, 120, "HERSEY_SIMPLEX", green, 2, 3);
+        // outputImg.putText("TEST", 25, 220, "HERSEY_SIMPLEX", blue, 4, 4);
+
+        // write to tmp detections image
+        outputImg.save(tmpDetectionsImg);
+
+        data.imgUrl = `/${ts}-${params.image.name}`;
+
+        return callback(null, data);
+
+      });
 
     });
 
