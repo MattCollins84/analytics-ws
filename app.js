e2ca7423d6c5278b54f62f95c7eff7df281ea947
MattCollins84 <matt@strikesandgutters.com>
2018-01-29 12:18:50 +0000

    First stab at config and classifiers

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const Steady = require('steady-api').Steady;
 const logger = require('morgan');
 const express = require('express');
+const Joi = require('joi');
+const Config = require('./lib/config');
+const config = new Config('custom');
 
 const app = new Steady({
   controllersDir: './controllers',
@@ -9,5 +12,11 @@ const app = new Steady({
   middleware: [
     logger('dev'),
     express.static('images')
+  ],
+  customTypes: [
+    {
+      name: "classes",
+      validation: Joi.array().items(config.availableClasses())
+    }
   ]
 });
--- controllers/image.js
+++ controllers/image.js
@@ -1,28 +1,19 @@
-const { exec } = require('child_process');
 const fs = require('fs');
 const cv = require('opencv');
-
-const nothing = (params, callback) => {
-
-  setTimeout(function() {
-    return callback(null, {})
-  }, 100)
-
-}
+const classify = require('../lib/classify');
+const Config = require('../lib/config');
+const config = new Config('custom');
+const async = require('async');
 
 // analyse a single image
-const singleImage = (params, callback) => {
+const singleImageClassify = (params, callback) => {
 
   // timestamp
   const ts = Date.now();
   // tmp filename of original image
   const tmpName = `/tmp/${ts}-${params.image.name}`;
-  // tmp filename of YOLO response data
-  const tmpOutput = `${tmpName}-output.txt`;
   // tmp filename of original image with detections drawn
   const tmpDetectionsImg = `./images/${ts}-${params.image.name}`;
-  // do we want process 2, 1, or 0
-  const processId = Date.now() % 5;
 
   // move the uploaded image to the tmp location
   params.image.mv(tmpName, (err) => {
@@ -35,69 +26,64 @@ const singleImage = (params, callback) => {
       ]
     });
 
-    // run YOLO on tmp image
-    exec(`/home/analytics/Projects/IPC/cvImgAnalytics ${tmpName} ${processId}`, (err, stdout, stderr) => {
-      
-      if (err) return callback({
-        status: 500,
-        errorMessage: "There was a problem analysing this file",
-        errors: [
-          err
-        ]
-      });
-
-      // parse output data
-      try {
-        data = JSON.parse(stdout.toString('utf8'))
-      } catch (e) {
-        return callback({
-          status: 500,
-          errorMessage: "An unexpected error occured",
-          errors: [
-            e
-          ]
+    const actions = [];
+    config.getClassifierCommands(params.classes).forEach(classifierParam => {
+      const func = function(callback) {
+        classify.singleImage(tmpName, classifierParam, (err, data) => {
+          if (err) return callback(err);
+          return callback(null, data);
         });
       }
+      actions.push(func);
+    })
 
-      // load the image into opencv
-      cv.readImage(tmpName, (err, outputImg) => {
-        
-        if (err) return callback({
-          status: 500,
-          errorMessage: "There was a problem loading the output image",
-          errors: [
-            err
-          ]
-        });
+    async.parallel(actions, (err, results) => {
 
-        if (params.annotate) {
-          // colours
-          const red = [0, 0, 255];
-          const green = [0, 255, 0];
-          const blue = [255, 0, 0];
-
-          // capture the detections and draw a rectangle for each
-          const detections = data.detections;
-          detections.forEach(detection => {
-            outputImg.rectangle([detection.x, detection.y], [detection.w, detection.h], red, 2);
-            outputImg.putText(detection.name, detection.x + 10, detection.y + 15, "HERSEY_SIMPLEX", red, 0.5, 2);
-          });
-
-          // outputImg.putText("TEST", 25, 20, "HERSEY_SIMPLEX", red, 1, 2);
-          // outputImg.putText("TEST", 25, 120, "HERSEY_SIMPLEX", green, 2, 3);
-          // outputImg.putText("TEST", 25, 220, "HERSEY_SIMPLEX", blue, 4, 4);
-
-          // write to tmp detections image
-          outputImg.save(tmpDetectionsImg);
-
-          data.imgUrl = `/${ts}-${params.image.name}`;
-        }
-
-        return callback(null, data);
-
-      });
+      if (err) return callback(err);
+      return callback(null, results);
 
     });
+
+
+      // // load the image into opencv
+      // cv.readImage(tmpName, (err, outputImg) => {
+        
+      //   if (err) return callback({
+      //     status: 500,
+      //     errorMessage: "There was a problem loading the output image",
+      //     errors: [
+      //       err
+      //     ]
+      //   });
+
+      //   if (params.annotate) {
+      //     // colours
+      //     const red = [0, 0, 255];
+      //     const green = [0, 255, 0];
+      //     const blue = [255, 0, 0];
+
+      //     // capture the detections and draw a rectangle for each
+      //     const detections = data.detections;
+      //     detections.forEach(detection => {
+      //       outputImg.rectangle([detection.x, detection.y], [detection.w, detection.h], red, 2);
+      //       outputImg.putText(detection.name, detection.x + 10, detection.y + 15, "HERSEY_SIMPLEX", red, 0.5, 2);
+      //     });
+
+      //     // outputImg.putText("TEST", 25, 20, "HERSEY_SIMPLEX", red, 1, 2);
+      //     // outputImg.putText("TEST", 25, 120, "HERSEY_SIMPLEX", green, 2, 3);
+      //     // outputImg.putText("TEST", 25, 220, "HERSEY_SIMPLEX", blue, 4, 4);
+
+      //     // write to tmp detections image
+      //     outputImg.save(tmpDetectionsImg);
+
+      //     data.imgUrl = `/${ts}-${params.image.name}`;
+      //   }
+
+      //   return callback(null, data);
+
+      // });
+
+    // });
 
   });
 
@@ -105,6 +91,5 @@ const singleImage = (params, callback) => {
 
 
 module.exports = {
-  singleImage,
-  nothing
+  singleImageClassify
 }
